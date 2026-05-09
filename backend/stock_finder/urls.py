28f9abcc838808758c3c8ba110b6acb3d9e669a5
Priyanshu Agarwal <priyanshu81212@gmail.com>
2026-05-09 11:27:09 +0530

    removed redundant /api

--- backend/stock_finder/urls.py
+++ backend/stock_finder/urls.py
@@ -4,13 +4,13 @@ from django.urls import include, path
 from .views import PingView
 
 urlpatterns = [
-    path("api/auth/", include("apps.owner_manager.urls")),
-    path("api/shops/", include("apps.shop_manager.urls")),
-    path("api/categories/", include("apps.inventory_manager.urls.categories")),
-    path("api/items/", include("apps.inventory_manager.urls.items")),
-    path("api/search/", include("apps.inventory_manager.urls.search")),
-    path("api/leads/", include("apps.lead_manager.urls.leads")),
-    path("api/reports/", include("apps.lead_manager.urls.reports")),
-    path("api/ping/", PingView.as_view(), name="ping"),
+    path("auth/", include("apps.owner_manager.urls")),
+    path("shops/", include("apps.shop_manager.urls")),
+    path("categories/", include("apps.inventory_manager.urls.categories")),
+    path("items/", include("apps.inventory_manager.urls.items")),
+    path("search/", include("apps.inventory_manager.urls.search")),
+    path("leads/", include("apps.lead_manager.urls.leads")),
+    path("reports/", include("apps.lead_manager.urls.reports")),
+    path("ping/", PingView.as_view(), name="ping"),
     path("admin/", admin.site.urls),
 ]
